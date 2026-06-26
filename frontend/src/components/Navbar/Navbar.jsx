135a6a9d44e1e22098d91a930c599e081b8db786
jionbiju <jionbiju@gmail.com>
2026-06-26 09:47:45 +0530

    navbar created

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
-
+import React from 'react';
+import './Navbar.css'; 
+import logo from '../../assets/logo.jpeg';
 const Navbar = () => {
   return (
-    <div className='container'>
-        <div className="img-box">
-            <img src="" alt="Logo" />
-            <h3>Water Vision <br />Fountains</h3>
+    <nav className="navbar">
+      <div className="navbar-container">
+        {/* Logo and Brand Section */}
+        <div className="logo-box">
+          <img src={logo} alt="Water Vision Fountains Logo" className="logo-img" />
+          <h2 className="brand-name">
+            WATER VISION <br />
+            <span className="brand-subtext">FOUNTAINS</span>
+          </h2>
         </div>
-        <div className="nav-items">
-            <ul>
-                <li>Home</li>
-                <li></li>
-            </ul>
-        </div>
-    </div>
-  )
-}
 
-export default Navbar
+        {/* Navigation Links */}
+        <div className="nav-menu">
+          <ul className="nav-list">
+            <li className="nav-item"><a href="#home" className="nav-link active">Home</a></li>
+            <li className="nav-item"><a href="#about" className="nav-link">About Us</a></li>
+            <li className="nav-item"><a href="#services" className="nav-link">Products & Services</a></li>
+            <li className="nav-item"><a href="#technology" className="nav-link">Technology</a></li>
+            <li className="nav-item"><a href="#projects" className="nav-link">Projects</a></li>
+            <li className="nav-item"><a href="#gallery" className="nav-link">Gallery</a></li>
+            <li className="nav-item"><a href="#contact" className="nav-link">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
